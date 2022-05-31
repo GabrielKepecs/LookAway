ad3817636b10a665f1a60cdbc29aacc4d6e79008
GabrielKepecs <gabkep@outlook.com>
2022-05-31 11:03:00 -0300

    Pause Menu; Jetpack funcional;

---
+++ Assets/Scripts/Jetpack.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jetpack : MonoBehaviour
+{
+    public int maxFuel = 4;
+    public float thrustForce = 0.5f;
+    public Rigidbody rbd;
+    public Transform groundedTransform;
+
+    [SerializeField]  private int curFuel;
+  
+
+    public void JetpackAct()
+    {
+        if (curFuel > 1f)
+        {
+            curFuel -= 1;
+            rbd.AddForce(rbd.transform.up * thrustForce, ForceMode.Impulse);
+
+        }
+        else if (Physics.Raycast(groundedTransform.position, Vector3.down, 0.05f, LayerMask.GetMask("Grounded")) && curFuel < maxFuel)
+        {
+            curFuel += 1;
+
+        }
+        else
+        {
+
+        }
+    }
+
+    void Start()
+    {
+        curFuel = maxFuel;
+    
+    }
+
+
+    void Update() { }
+   
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    
+
+    public void PauseButton()
+    {
+        if (GameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+}
+
+
+
+
